08aac441ebf264f2f203fac7ebb4eb22cc153813
nitzankatz2 <nitzankatz22@gmail.com>
2019-01-25 19:00:09 +0200

    continue n-way-k-shot

--- n_way_k_shot.py
+++ n_way_k_shot.py
@@ -1,22 +1,38 @@
 import torch
 import os
 import random
+from torch.utils.data import Dataset
 
 
 def main(root_dir, N, k):
     classes_dirs = get_n_classes_dirs(N, root_dir)
     comparable_lists, test_lists = split_classes(classes_dirs, k)
+    acc = get_accuracy(comparable_lists, test_lists)
+    a = 3
+
+
+def get_accuracy(comparable_lists, test_lists):
     total_correct, total_samples = 0, 0
-    for class_num, class_test_images in enumerate(test_lists):
-        num_correct, num_samples = test_class(class_test_images, class_num, comparable_lists)
+    comparable_images = get_images_for_lists(comparable_lists)
+    for class_num, class_test_list in enumerate(test_lists):
+        num_correct, num_samples = test_class(class_test_list, class_num, comparable_images)
         total_correct += num_correct
         total_samples += num_samples
-    acc = total_correct / total_samples
-    a=3
+    return total_correct / total_samples
 
 
-def test_class(class_test_images, class_num, comparable_lists):
-    return 1, len(class_test_images)
+def get_images_for_lists(lists):
+    for list in lists[1:]:
+        assert len(lists[0]) == len(list)
+    classes_batches = [list_to_batch(list) for list in lists]
+    return comparable_images
+
+
+def list_to_batch
+
+
+def test_class(class_test_lists, class_num, comparable_images):
+    return 1, len(class_test_lists)
 
 
 def get_n_classes_dirs(N, root_dir):
@@ -53,6 +69,22 @@ def choose_n_from_list(list, n):
     n_list = list[:n]
     rest_list = list[n:]
     return n_list, rest_list
+
+
+class ListDataset(Dataset):
+    def __init__(self, elem_list, transform,load):
+        super(ListDataset, self).__init__()
+        self.list = elem_list
+        self.load = load
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.list)
+
+    def __getitem__(self, idx):
+        super(ListDataset, self).__getitem__(idx)
+        im = self.load(self.list[idx])
+        return self.transform(im)
 
 
 if __name__ == '__main__':
