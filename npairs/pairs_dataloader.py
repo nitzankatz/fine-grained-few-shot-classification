2228c60affe5410ff5eb72378120b4496ace9b0f
nitzankatz2 <nitzankatz22@gmail.com>
2019-02-22 11:25:47 +0200

    finished dataset for pairs

---
+++ npairs/pairs_dataloader.py
@@ -0,0 +1,47 @@
+from torch.utils.data import Dataset, DataLoader
+import os
+import random
+from PIL import Image
+import numpy as np
+from utils import get_val_transforms
+
+def get_files_list(base_dir, class_dir):
+    class_path = os.path.join(base_dir, class_dir)
+    files_names = os.listdir(class_path)
+    return [os.path.join(class_path, file_name) for file_name in files_names]
+
+class PairsDataSet(Dataset):
+    def __init__(self, base_dir, transform):
+        super(Dataset, self).__init__()
+        self.transform = transform
+        self.classes = os.listdir(base_dir)
+        self.samples = [get_files_list(base_dir, class_dir) for class_dir in self.classes]
+
+    def get_image(self, path):
+        with open(path, 'rb') as f:
+            img = Image.open(f)
+            img = img.convert('RGB')
+            # img = np.asarray(img)
+            return self.transform(img)
+
+    def __getitem__(self, idx):
+        if idx >= len(self.classes):
+            raise Exception('idx should be 0<= idx < len(classes)')
+        curr_class_samples = self.samples[idx]
+        inds_in_class = random.sample(range(len(curr_class_samples)), 2)
+        anchor = self.get_image(curr_class_samples[inds_in_class[0]])
+        positive = self.get_image(curr_class_samples[inds_in_class[1]])
+        label = idx
+        return anchor, positive, label
+
+    def __len__(self):
+        return len(self.classes)
+
+
+if __name__ == '__main__':
+    p = PairsDataSet(os.path.join('data', 'CUB_200_2011', 'images', 'val'), get_val_transforms(input_size=224))
+    loader = DataLoader(p, batch_size=5, shuffle=True)
+    for x in loader:
+        print(x)
+
+    pass
