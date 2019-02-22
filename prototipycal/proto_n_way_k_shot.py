2780dc943aa6920d08f0817fae5d655b5570ec8f
nitzankatz2 <nitzankatz22@gmail.com>
2019-02-22 15:49:05 +0200

    made protonway into test script

--- prototipycal/proto_n_way_k_shot.py
+++ prototipycal/proto_n_way_k_shot.py
@@ -5,14 +5,16 @@ from prototipycal.proto_sampler import PrototypicalBatchSampler
 from prototipycal.proto_loss import PrototypicalLoss
 from basenets.squeezenet import SqueezeNet
 import os
+import sys
 
-def proto_n_way_k_shot(val_dir, N, k,net, input_size=224, num_support=5):
+
+def proto_n_way_k_shot(val_dir, N, k, net, input_size=224, num_support=5):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     val_trans_list = get_val_transforms(input_size=input_size)
     val_dataset = datasets.ImageFolder(
         val_dir, val_trans_list)
     classes = [sample_tuple[1] for sample_tuple in val_dataset.samples]
-    sampler = PrototypicalBatchSampler(classes, N, k + num_support, 10)
+    sampler = PrototypicalBatchSampler(classes, N, k + num_support, 600)
 
     val_loader = torch.utils.data.DataLoader(
         val_dataset,
@@ -27,22 +29,18 @@ def proto_n_way_k_shot(val_dir, N, k,net, input_size=224, num_support=5):
         num_acc += 1
     return (sum_acc / num_acc)
 
-if __name__ == '__main__':
-    # root_dir = r"C:\temp\tempfordeep"
-    root_dir = r'C:\dev\studies\deepLearning\fine-grained-few-shot-calssification\data\CUB_200_2011\images\val'
-    # root_dir = r'C:\temp\tempfordeep4'
-    # for seed in range(3):
-    #     print(seed)
-    #     split_check(root_dir, 2, 1, seed)
 
-    # net = MobileNetV2()
-    # state_dict = torch.load(os.path.join('weights', 'mobilenet_v2.pth'), map_location=lambda storage, loc: storage)
+if __name__ == '__main__':
+    root_dir = os.path.join('data', 'CUB_200_2011', 'images', 'test')
+    # weight_path = 'random'
+    # weight_path = os.path.join('weights', 'squeezenet_class.pth')
+    weight_path = sys.argv[1]
 
     net = SqueezeNet()
-    state_dict = torch.load(os.path.join('weights', 'squeezenet1_0-a815701f.pth'),
+    state_dict = torch.load(weight_path,
                             map_location=lambda storage, loc: storage)
 
     net.load_state_dict(state_dict)
     net.eval()
-    acc = proto_n_way_k_shot(root_dir, 5, 5,net)
+    acc = proto_n_way_k_shot(root_dir, 5, 5, net)
     print(acc)
