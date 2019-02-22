b13bfe518721dd93816d2bde693b27099749d12e
nitzankatz2 <nitzankatz22@gmail.com>
2019-02-22 16:05:06 +0200

    fixed device in proto nk

--- prototipycal/proto_n_way_k_shot.py
+++ prototipycal/proto_n_way_k_shot.py
@@ -10,7 +10,6 @@ import sys
 
 def proto_n_way_k_shot(val_dir, N, k, net, input_size=224, num_support=5):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    print(device)
     net = net.to(device)
     val_trans_list = get_val_transforms(input_size=input_size)
     val_dataset = datasets.ImageFolder(
@@ -43,7 +42,6 @@ if __name__ == '__main__':
                             map_location=lambda storage, loc: storage)
 
     net.load_state_dict(state_dict)
-    net = net.to(device)
     net.eval()
     acc = proto_n_way_k_shot(root_dir, 5, 5, net)
     print(acc)
