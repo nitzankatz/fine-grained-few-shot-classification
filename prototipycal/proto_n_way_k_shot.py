cfcca8b573848beece86761c797d1c8f91918714
nitzankatz2 <nitzankatz22@gmail.com>
2019-02-22 16:29:33 +0200

    fixed device in proto nk

--- prototipycal/proto_n_way_k_shot.py
+++ prototipycal/proto_n_way_k_shot.py
@@ -8,27 +8,28 @@ import os
 import sys
 
 
-def proto_n_way_k_shot(val_dir, N, k, net, input_size=224, num_support=5):
+def proto_n_way_k_shot(val_dir, N, k, net, input_size=224, num_query=10):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     net = net.to(device)
     val_trans_list = get_val_transforms(input_size=input_size)
     val_dataset = datasets.ImageFolder(
         val_dir, val_trans_list)
     classes = [sample_tuple[1] for sample_tuple in val_dataset.samples]
-    sampler = PrototypicalBatchSampler(classes, N, k + num_support, 600)
+    sampler = PrototypicalBatchSampler(classes, N, k + num_query, 10)
 
     val_loader = torch.utils.data.DataLoader(
         val_dataset,
         num_workers=1, batch_sampler=sampler)  # , pin_memory=True)
-    loss_func = PrototypicalLoss(n_support=5)
-    sum_acc = 0
-    num_acc = 0
+    loss_func = PrototypicalLoss(n_support=k)
+    # sum_acc = 0
+    # num_acc = 0
+    acc_list = []
     for x, y in iter(val_loader):
         emb = net.embed(x.to(device))
         loss, acc = loss_func(emb, y.to(device))
-        sum_acc += acc
-        num_acc += 1
-    return (sum_acc / num_acc)
+        acc_list.append(acc)
+    accs = torch.stack(acc_list)
+    return torch.mean(accs), torch.std(accs)
 
 
 if __name__ == '__main__':
@@ -38,10 +39,11 @@ if __name__ == '__main__':
     weight_path = sys.argv[1]
 
     net = SqueezeNet()
-    state_dict = torch.load(weight_path,
-                            map_location=lambda storage, loc: storage)
+    # state_dict = torch.load(weight_path,
+    #                         map_location=lambda storage, loc: storage)
 
-    net.load_state_dict(state_dict)
+    # net.load_state_dict(state_dict)
     net.eval()
-    acc = proto_n_way_k_shot(root_dir, 5, 5, net)
-    print(acc)
+    mean, std = proto_n_way_k_shot(root_dir, 5, 5, net)
+    print('mean: ' + str(mean))
+    print('std: ' + str(std))
