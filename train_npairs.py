d806b539d11a6ae7746d8d8a13180755fc01722e
Liorm <lior.magram10@gmail.com>
2019-02-23 14:50:58 +0200

    learning rate changed to 3.3e-5

--- train_npairs.py
+++ train_npairs.py
@@ -26,7 +26,7 @@ def train(net, data_loader, loss_fn, experiment_name, valdir):
         experiment_num = 1
     else:
         experiment_num = max(prevoius_experiments_numeric) + 1
-    optimizer = torch.optim.SGD(net.parameters(), lr=1e-5, momentum=0.9, weight_decay=1e-3)
+    optimizer = torch.optim.SGD(net.parameters(), lr=3.3e-5, momentum=0.9, weight_decay=1e-3)
 
     # SummaryWriter encapsulates everything
     writer = SummaryWriter(os.path.join(main_tesnorboard_dir, str(experiment_num)))
